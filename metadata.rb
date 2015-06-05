f5cf110c78a31df0f94e5dd8353e237f5fbd9591
James Taylor <jamestaylr@gmail.com>
2015-06-04 20:24:32 -0400

    Added Java dependency and installation recipe
    Chef now installs a given version of Java on the server.

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,5 @@ license          'MIT'
 description      'Installs/Configures Spigot Installations of Minecraft'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'java'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,6 @@
 # Cookbook Name:: spigot
 # Recipe:: default
 #
+
+# Installs the specified Java version on the server
+include_recipe "java::#{node['java']['version']}"
