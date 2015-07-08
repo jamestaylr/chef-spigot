2e80b86ce1d69fd1f842d997f24335a0b36fc180
James Taylor <jamestaylr@gmail.com>
2015-07-08 00:06:54 -0400

    Modified Java 7 installation cookbook call
    Java 7 is now installed and correctly linked on the system.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
-node.default['java']['version'] = 'default'
+
+node.default['spigot']['directory'] = "/srv/minecraft"
+
+override[:java][:openjdk_packages] = [
+  "openjdk-7-jdk", "openjdk-7-jre-headless"
+]
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,14 @@
 #
 
 # Installs the specified Java version on the server
-include_recipe "java::#{node['java']['version']}"
+
+include_recipe "apt"
+include_recipe "java"
+
+link "/etc/alternatives/java" do
+  to "/usr/lib/jvm/java-7-openjdk-amd64/bin/java"
+end
+
+link "/etc/alternatives/javac" do
+  to "/usr/lib/jvm/java-7-openjdk-amd64/bin/javac"
+end
