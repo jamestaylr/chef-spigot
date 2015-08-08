e850aa3f506b4d343b3919d9fffe72ad3566b073
James Taylor <jamestaylr@gmail.com>
2015-08-08 17:22:00 -0400

    Added Spigot ops, whitelist, banned-player, and banned-ip attributes

---
+++ attributes/spigot.rb
@@ -0,0 +1,30 @@
+
+node.default['spigot']['download'] = "http://getspigot.org/spigot18/spigot_server.jar"
+
+node.default['ops'] = [
+  {
+    "name" => "",
+    "UUID" => "",
+  }
+]
+
+node.default['white-list'] = [
+  {
+    "username" => "",
+    "UUID" => ""
+  }
+]
+
+node.default['banned-players'] = [
+  {
+    "username" => "",
+    "UUID" => ""
+  }
+]
+
+node.default['banned-ips'] = [
+  {
+    "username" => "",
+    "UUID" => ""
+  }
+]
