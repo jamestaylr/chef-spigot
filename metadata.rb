29f0b76a2529aacabfe82a61483e170764cd43aa
James Taylor <jamestaylr@gmail.com>
2015-09-16 12:18:25 -0400

    Incremented package version number for minor release

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'spigot'
 license          'MIT'
 description      'Installs/Configures Spigot Installations of Minecraft'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w(ubuntu debian redhat centos).each do |os|
   supports os
